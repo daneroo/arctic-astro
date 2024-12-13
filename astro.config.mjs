36eaf964239c10515add53343d1c38075aaf20d5
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-12-13 04:46:48 -0500

    Fix typo in astro.config.mjs comment regarding redirect delay. This change corrects the word "cases" to "causes" for improved clarity in the configuration documentation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     defaultLocale: "fr", // Note: If changing this, also update src/pages/index.astro redirect
     locales: ["en", "fr"],
     routing: {
-      // making this true works, but cases a 2 second delay to redirect / to /{defaultLocale}
+      // making this true works, but causes a 2 second delay to redirect / to /{defaultLocale}
       // so instead we set it to false and made a custom index.astro page to do the redirect
       prefixDefaultLocale: false,
     },
