c5f3097ca3c206b601d68492ca0c129ce071a393
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-12-13 04:39:32 -0500

    Update locale handling in astro.config.mjs and index.astro to eliminate redirect delay. Set prefixDefaultLocale to false and implement a custom redirect page for smoother navigation to the default locale. This change improves user experience by avoiding the built-in delay while maintaining support for English and French locales.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,9 @@ export default defineConfig({
     defaultLocale: "fr", // Note: If changing this, also update src/pages/index.astro redirect
     locales: ["en", "fr"],
     routing: {
-      prefixDefaultLocale: true,
+      // making this true works, but cases a 2 second delay to redirect / to /{defaultLocale}
+      // so instead we set it to false and made a custom index.astro page to do the redirect
+      prefixDefaultLocale: false,
     },
   },
 });
